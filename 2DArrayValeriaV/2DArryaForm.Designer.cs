3c93a2344a51b3071a5ed5fbaaea34835079a528
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2019-01-11 09:53:43 -0500

    Initial commit

---
+++ 2DArrayValeriaV/2DArryaForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace _2DArrayValeriaV
+{
+    partial class frm2DArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudLength = new System.Windows.Forms.NumericUpDown();
+            this.nudWidth = new System.Windows.Forms.NumericUpDown();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtArray = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudLength
+            // 
+            this.nudLength.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudLength.Location = new System.Drawing.Point(120, 106);
+            this.nudLength.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.nudLength.Name = "nudLength";
+            this.nudLength.Size = new System.Drawing.Size(52, 32);
+            this.nudLength.TabIndex = 0;
+            // 
+            // nudWidth
+            // 
+            this.nudWidth.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudWidth.Location = new System.Drawing.Point(120, 49);
+            this.nudWidth.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudWidth.Name = "nudWidth";
+            this.nudWidth.Size = new System.Drawing.Size(52, 32);
+            this.nudWidth.TabIndex = 1;
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.lblHeight.Location = new System.Drawing.Point(43, 49);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(59, 25);
+            this.lblHeight.TabIndex = 2;
+            this.lblHeight.Text = "Width";
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLength.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.lblLength.Location = new System.Drawing.Point(43, 108);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(66, 25);
+            this.lblLength.TabIndex = 3;
+            this.lblLength.Text = "Length";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.btnCalculate.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.ForeColor = System.Drawing.Color.White;
+            this.btnCalculate.Location = new System.Drawing.Point(202, 75);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(99, 38);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtArray
+            // 
+            this.txtArray.Location = new System.Drawing.Point(103, 144);
+            this.txtArray.MaximumSize = new System.Drawing.Size(200, 200);
+            this.txtArray.Multiline = true;
+            this.txtArray.Name = "txtArray";
+            this.txtArray.Size = new System.Drawing.Size(129, 146);
+            this.txtArray.TabIndex = 5;
+            this.txtArray.TextChanged += new System.EventHandler(this.txtArray_TextChanged);
+            // 
+            // frm2DArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 302);
+            this.Controls.Add(this.txtArray);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.nudWidth);
+            this.Controls.Add(this.nudLength);
+            this.Name = "frm2DArray";
+            this.Text = "2D Array by Valeria V";
+            ((System.ComponentModel.ISupportInitialize)(this.nudLength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudLength;
+        private System.Windows.Forms.NumericUpDown nudWidth;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtArray;
+    }
+}
+
---
+++ 2DArrayValeriaV/2DArryaForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms;
+
+namespace _2DArrayValeriaV
+{
+    public partial class frm2DArray : Form
+    {
+        public frm2DArray()
+        {
+            InitializeComponent();
+        }
+
+        //Function: CalcAverage
+        //Input: int[,]tmpArray, int width, int length
+        //Output: double average
+        //Description: this function calcualtes the average of all values in the array
+        private double CalcAverage(int[,]tmpArray, int width, int length)
+        {
+            //declare local varaibles
+            double average = 0;
+            double numOfVal = 0;
+            double sum = 0;
+
+            //get the length of the array
+            numOfVal = tmpArray.Length;
+
+            //loop through each element in the width
+            for (int widthCounter = 0; widthCounter < width; widthCounter++)
+            {
+                //loop through each element in the length
+                for (int lengthCounter = 0; lengthCounter < length; lengthCounter++)
+                {
+                    //calculate the sum of the values in the array
+                    sum = sum + tmpArray[widthCounter, lengthCounter];
+                }
+
+
+            }
+            //calcualte the average
+            average = sum / numOfVal;
+            //return the average
+            return average;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+
+            //declare local variable
+            int width;
+            int length;
+            int aRandNum;
+            Random generateRandNum = new Random();
+            string pieceText = "";
+
+            width = Convert.ToInt16(nudWidth.Value);
+            length = Convert.ToInt16(nudLength.Value);
+            //create a 2D array
+            int[,] randArray = new int[width, length];
+
+            //loop through each element in the width
+            for (int widthCounter = 0; widthCounter < width; widthCounter++ )
+            {
+                //loop through each element in the length
+                for (int lengthCounter = 0; lengthCounter < length; lengthCounter++)
+                {
+                    //generate a random number
+                    aRandNum = generateRandNum.Next(1, 10);
+                    //add the random number to the array
+                    randArray[widthCounter, lengthCounter] = aRandNum;
+                    //display it in the textbox
+                    pieceText = pieceText + " " + aRandNum  ;
+                }
+                //format the string
+                pieceText = pieceText + "\r" + "\n";
+
+            }
+            //display in the textbox
+            txtArray.Text = pieceText;
+
+            double displayValue = CalcAverage(randArray, width, length);
+            MessageBox.Show("The average of all numbers is " + displayValue);
+            
+        }
+
+        private void txtArray_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
